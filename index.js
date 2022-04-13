38b8e45ac0d5a74fbbeba6ba48a7a9e711e4285f
Cristian <Entenduu@gmail.com>
2022-04-13 00:37:10 -0400

    commit tree a83f55c2bd36667b7daa9e68eea7864d60de62f3

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x * y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+function increment(n){
+    let number = n;
+    number++;
+    return number;
+}
+
+function decrement(n){
+    let number = n;
+    number--;
+    return number;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
